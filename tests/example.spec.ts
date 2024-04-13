a811e1e28b330e355c4cfbb1ecd5663ee31a785f
Fanduandan <15658866967@163.com>
2024-04-13 20:36:49 +0800

    test: add test script

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,21 +1,27 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, chromium } from '@playwright/test';
 
 
-test('get started link', async ({ page }) => {
-    // cy.get('h4.subapp-loading').contains('Loading...');
 
-    // cy.wait(1000);
+test('get started link', async () => {
+  // cy.get('h4.subapp-loading').contains('Loading...');
 
-    // cy.get('div.app-title]').contains('React Demo');
+  // cy.wait(1000);
 
-    // cy.url().should('include', 'react16');
+  // cy.get('div.app-title]').contains('React Demo');
+
+  // cy.url().should('include', 'react16');
 
 
-  await page.goto('http://localhost:7099');
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.goto('http://localhost:7099/');
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.waitForSelector('h4.subapp-loading:has-text("Loading...")');
+  await page.waitForTimeout(3000);
+  await page.waitForSelector('div.app-title:has-text("React Demo")');
+
+  expect(page.url()).toContain('react16');
+
+  await browser.close();
 });
